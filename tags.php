d870beaec94b915a051705e7c5401a9f79f4b43e
Fabrício Ferracioli <fabricioferracioli@gmail.com>
2009-12-15 16:36:24 -0200

    the tags.php file have the html 4.01 tags to be generated by cakephp htmlhelper. not all tags in the file yet.

---
+++ tags.php
@@ -0,0 +1,14 @@
+<?php
+
+    $tags = array(
+        'charset' => '<meta http-equiv="Content-Type" content="text/html; charset=%s">',
+        'css' => '<link rel="%s" type="text/css" href="%s">',
+        'hidden' => '<input type="hidden" name="%s" %s>',
+        'image' => '<img src="%s" %s>',
+        'input' => '<input name="%s" %s>',
+        'metalink' => '<link href="%s" %s>',
+        'password' => '<input type="password" name="%s" %s>',
+        'submit' => '<input type="submit" %s>'
+    );
+
+?>
